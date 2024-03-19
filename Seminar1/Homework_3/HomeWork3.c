1a7816d31f372c6962933997d8ff742e475d6124
Kinesis <lordkinesis@naver.com>
2024-03-19 20:02:47 +0900

    Edit Operator.c and Condition.c Files Name

---
+++ Seminar1/Homework_3/HomeWork3.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+// 33±â 24¹Ú±âÈ«
+int main() {
+
+	
+	return 0;
+}
---
+++ Seminar1/Project2/Condition.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int num;
+	printf("정수를 입력하세요: ");
+	scanf("%d", &num);
+
+	if (num == 10) {
+		printf("입력값은 10이다.\n");
+	}else if (num == 20) {
+		printf("입력값은 10이다.\n");
+	}else if (num == 20) {
+		printf("입력값은 10이다.\n");
+	}else{
+		printf("10도 20도 아닙니다.\n");
+	}
+
+
+	return 0;
+}
