84ba081921842ca642f4f542398d9f131242d76a
Kinesis <lordkinesis@naver.com>
2024-03-26 19:28:24 +0900

    Adding CallbyValue.c

---
+++ Seminar3/Project2/CallbyValue.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void swap(int a, int b);
+
+int main() {
+
+	int m = 2, n = 9;
+
+	printf(" [Àü] a = %d b = %d\n", m, n);
+
+	swap(m, n);
+
+	printf(" [ÈÄ] a = %d b = %d\n", m, n);
+
+	return 0;
+}
+
+
+void swap(int a, int b) {
+	int tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
